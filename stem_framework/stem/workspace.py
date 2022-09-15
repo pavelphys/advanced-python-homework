e9c138cf3918bb4edadff0700ffe9fa70aa72470
pavelphys <88884697+pavelphys@users.noreply.github.com>
2022-09-15 03:28:09 +0300

    Update workspace.py

---
+++ stem_framework/stem/workspace.py
@@ -0,0 +1,4 @@
+'''
+Modularity is the standard for software in Computer science, but monolithic systems are still very often used in experimental physics.
+Monolithic systems are mostly very complex in terms of support and upgrades.
+'''
